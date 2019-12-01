aa6ddcc41497af51a3ebe7db31e66a6e21879573
Breno Salles <breno-salles@hotmail.com>
2019-12-01 20:54:27 +0000

    Renamed uuid_generator.sql to b_uuid_generator.sql

---
+++ database/b_uuid_generator.sql
@@ -0,0 +1,19 @@
+-- Activate pgcrypto extension in order to use gen_random_bytes function
+CREATE EXTENSION pgcrypto; 
+
+-- This will create a random uuid with length=size
+CREATE OR REPLACE FUNCTION generate_uid(size INT) RETURNS TEXT AS $$
+DECLARE
+  characters TEXT := 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
+  bytes BYTEA := gen_random_bytes(size);
+  l INT := length(characters);
+  i INT := 0;
+  output TEXT := '';
+BEGIN
+  WHILE i < size LOOP
+    output := output || substr(characters, get_byte(bytes, i) % l + 1, 1);
+    i := i + 1;
+  END LOOP;
+  RETURN output;
+END;
+$$ LANGUAGE plpgsql VOLATILE;
