f06b02720feeed98af532981a8a50a11626b413c
diogofalken <diogofalken0@gmail.com>
2020-01-15 20:51:35 +0000

    Ended the structure of Main page

---
+++ frontend/src/pages/Main.js
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import './Main.css';
+
+import api from '../services/api';
+
+export default function Main({ match, history }) {
+    const [restaurante, setRestaurante] = useState({});
+
+
+    useEffect(() => {
+        async function loadInfo() {
+            const response = await api.get(`/restaurantes/${match.params.id}`);
+            setRestaurante(response.data["message"]);
+            console.log(response.data["message"]);
+        }
+
+        loadInfo();
+    }, [match.params.id]);
+
+    async function handleEmentas() {
+        const response = await api.get(`/ementas`);
+
+        const aux = response.data["message"].filter(ementa => ementa.cod_restaurante === match.params.id);
+
+        console.log(aux);
+    }
+
+    return (
+        <div className='main-container'>
+            <Link to='/' style={{ textDecoration: 'none' }}>
+                <h1>{restaurante.designacao}</h1>
+            </Link>
+            <ul>
+                <li><button onClick={() => history.push(`/ementas/${match.params.id}`)}>Ementas</button></li>
+                <li><button onClick={() => history.push(`/funcionarios/${match.params.id}`)}>Funcionarios</button></li>
+                <li><button onClick={() => history.push(`/itens/${match.params.id}`)}>Itens</button></li>
+                <li><button onClick={() => history.push(`/alergias/${match.params.id}`)}>Alergias</button></li>
+                <li><button onClick={() => history.push(`/consumos/${match.params.id}`)}>Consumos</button></li>
+            </ul>
+        </div>
+    );
+}
